f477d7212f4b8c94549594db7b43494c7262602e
ssixteenvalvee <nb-775@yandex.ru>
2025-09-16 09:39:46 +0300

    Added a math function nb

--- practicing.py
+++ practicing.py
@@ -2,3 +2,5 @@ print('hello world')
 
 first_num = int(input('Введите первое число: '))
 second_num = int(input('Введите второе число '))
+
+print(f'Sum of squares of your numbers = {first_num**2 + second_num**2}')
