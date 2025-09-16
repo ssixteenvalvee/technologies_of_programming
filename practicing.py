efc33d213f776be7db544b5d4272f8f186c10f5f
ssixteenvalvee <nb-775@yandex.ru>
2025-09-16 10:23:26 +0300

    Added a reversed math function tst

--- practicing.py
+++ practicing.py
@@ -2,3 +2,5 @@ print('hello world')
 
 first_num = int(input('Введите первое число: '))
 second_num = int(input('Введите второе число '))
+
+print(f'difference of squares of your numbers = {first_num**2 - second_num**2}')
