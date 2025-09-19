a8a87173a5c13e84bb8f63f283d82816fc079c20
ssixteenvalvee <nb-775@yandex.ru>
2025-09-19 09:36:30 +0300

    Added a difference and created ogurec.txt

--- practicing.py
+++ practicing.py
@@ -4,3 +4,5 @@ first_num = int(input('Введите первое число: '))
 second_num = int(input('Введите второе число '))
 
 print(f'difference of squares of your numbers = {first_num**2 - second_num**2}')
+
+differnse_1 = first_num - second_num
